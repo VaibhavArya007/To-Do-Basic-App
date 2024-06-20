b38b219956dcce2f381908e6283de715e678dfd0
Vaibhav Arya <vaibhavaryavns007@gmail.com>
2024-06-21 01:23:52 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 // mongodb+srv://vaibhavaryavns007:FRQ8fMdvkkczTvL0@cluster0.dvynsag.mongodb.net/todos
 
-mongoose.connect("mongodb+srv://vaibhavaryavns007:FRQ8fMdvkkczTvL0@cluster0.dvynsag.mongodb.net/todos")
+mongoose.connect("your mongoose link here")
 
 const todoSchema= mongoose.Schema({
     title: String,
